1057b126961dffb3f24722b8b0da21325c0cae20
JeevanRajpurohit <140495760+JeevanRajpurohit@users.noreply.github.com>
2024-02-22 13:40:31 +0530

    structure

---
+++ cash_Transaction_min.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+//money splitwise algorithm
+//graph implementation
+//output=contain all details with transaction 
+
+//rakesh pay= 100 to jevven
+//jeevan pay =50 to bhavesh
+//bhavesh pay =40 to rakesh
+
+//our algo make above transaction sort to
+//rakesh pay=50 to jeevan
+//rakesh pay =10 to bhavesh
+
+int main(){
+    int numberOfTransaction,friends;
+    cin>>numberOfTransaction;
+    cin>>friends;
+
+
+}
